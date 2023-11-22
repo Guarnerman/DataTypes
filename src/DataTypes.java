b3e229c90591e63a678f27d5c6acecddfa4b1e10
guarnerman <gs.ulises18@gmail.com>
2023-11-22 00:04:04 -0600

    Demostración de tipos de datos desde Java 10 en adelante y su uso de var

---
+++ src/DataTypes.java
@@ -0,0 +1,22 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int n = 1234567890;
+
+        long nL = 12345678901L;
+
+        double nD = 123.456123456;
+        float nF = 123.456F;
+
+        var salary = 1000;
+        //pension 3%
+        var pension = salary * 0.03;
+        var totalSalary = salary - pension;
+
+        System.out.println(salary);
+        System.out.println(pension);
+        System.out.println(totalSalary);
+
+        var employeeName = "Votitikzo";
+        System.out.println("EMPLOYEE: " + employeeName + " SALARY: " + totalSalary);
+    }
+}
